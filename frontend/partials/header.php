8bf2e7afd2ad127484cb54a000cbb938a7530591
thaiD20085 <tqthaid20085@cusc.ctu.edu.vn>
2021-02-27 20:59:02 +0700

    final1

--- frontend/partials/header.php
+++ frontend/partials/header.php
@@ -9,9 +9,6 @@
                     <li class="nav-item">
                         <a href="/project-D20085/index.php?direct=timcuahang" class="px-2 py-3">Tìm cửa hàng</a>
                     </li>
-                    <li class="nav-item">
-                        <a href="/project-D20085/index.php?direct=kiemtradonhang" class="px-2 py-3">Kiểm tra đơn hàng</a>
-                    </li>
                 </ul>
 
                 <?php if (!isset($_SESSION['kh_tendangnhap_logged'])) : ?>
@@ -40,7 +37,7 @@
     </nav>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container w-100">
-            <a class="navbar-brand" href="index.php">
+            <a class="navbar-brand" href="/project-D20085/index.php">
                 <h2 class="font-weight-bold" style="color:#FCA311">Linh kiện vi tính</h2>
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -49,7 +46,7 @@
             <div class="collapse navbar-collapse ml-3" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto font-weight-bold nav-bottom">
                     <li class="nav-item active">
-                        <a class="nav-link" href="index.php">TRANG CHỦ<span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="/project-D20085/index.php">TRANG CHỦ<span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/project-D20085/index.php?direct=linhkien">LINH KIỆN</a>
@@ -66,7 +63,7 @@
                 </ul>
                 <form id="search" class="d-tablet-none">
                     <label for="txtSearch"><button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button></label>
-                    <input type="text" name="txtSearch" id="txtSearch" placeholder="Tìm kiếm" />
+                    <input type="text" name="txtSearch" id="txtSearch" placeholder="Tìm kiếm" readonly/>
                     <button type="reset"><i class="fa fa-times-circle text-danger" aria-hidden="true"></i></button>
                 </form>
                 <a href="/project-D20085/index.php?direct=giohang" id="cart"><i class="fa fa-shopping-cart " aria-hidden="true"></i></a>
